61795b8765811b6a2beba019bd83b1c7a5500d35
Jaikumar Yadav <112802959+jkgit08@users.noreply.github.com>
2022-12-17 09:19:31 +0530

    Create Anagram.cpp

---
+++ Anagram.cpp
@@ -0,0 +1,24 @@
+#include<iostream.h>
+#include<conio.h>
+ 
+int isAnagram(string s, string t) {
+        int n=s.length();
+        int m=t.length();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(s[i]==t[j])
+                 return 1;  
+                 break;
+            }
+        }return 0;
+}
+
+void main(){
+  clrscr();
+   string s="anagram";
+   string t="nagaram";
+   int j=isAnagram(s,t);
+   cout<<j;
+  
+  getch();
+}
